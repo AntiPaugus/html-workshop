33ec3dc062e72cc85a763658c4d4f8750e750654
AntiPaugus <anti.paugus@gmail.com>
2024-02-08 13:30:06 +0200

    veebileht

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // addEventListener "kuulab", kui nupu peale vajutatakse.
 document.getElementById("button").addEventListener("click", function () {
+    document.getElementById("textBox").style.color = "red";
+    document.body.style.background = "blue"
 
 })
